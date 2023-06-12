14fa4f0764985e88cd10a17549225fec396d7fbc
LDH1103 <ldh517525@gmail.com>
2023-06-12 21:48:20 +0900

    0612 udt footer 이동호

---
+++ PHP-506-airplane/public/js/common.js
@@ -0,0 +1,29 @@
+// DOMContentLoaded 이벤트(페이지 로딩)가 발생했을 때 실행
+window.addEventListener('DOMContentLoaded', function() {
+    let footer = document.querySelector('.footer');
+    let divContentsMain = document.querySelector('.divContentsMain');
+
+    function checkFooterPosition() {
+        // 현재 창의 높이
+        let windowHeight = window.innerHeight;
+        // footer의 높이
+        let footerHeight = footer.offsetHeight;
+        let contentHeight = divContentsMain.scrollHeight;
+
+        // footer가 화면 하단에 고정되어야 하는지 확인
+        // 90(px)은 추가여백(만약 스크롤은 없는 높이지만, 컨테츠는 화면 가득 차있을경우를 위해 넣어줌)
+        if (contentHeight + footerHeight + 90 < windowHeight) {
+            footer.classList.add('footerFixed');
+        } else {
+            footer.classList.remove('footerFixed');
+        }
+    }
+
+    // 페이지 로딩 시 footer의 위치를 조정
+    checkFooterPosition();
+
+    // 윈도우 크기 변경 시 footer의 위치를 다시 확인후 조정
+    window.addEventListener('resize', function() {
+        checkFooterPosition();
+    });
+});
--- PHP-506-airplane/resources/views/layout/layout.blade.php
+++ PHP-506-airplane/resources/views/layout/layout.blade.php
@@ -12,10 +12,13 @@
 <body>
     @include('layout.inc.header')
 
+    <div class="divContentsMain">
     @yield('contents')
+    </div>
 
     @include('layout.inc.footer')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="{{asset('js/common.js')}}"></script>
     @yield('js')
 </body>
 </html>
