7493597ea295903a94fcf6ab36bc700baaf77061
ldh1103 <ldh517525@gmail.com>
2023-06-12 12:17:05 +0900

    0612 layout

---
+++ PHP-506-airplane/resources/views/layout/example.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.layout')
+
+@section('title', '타이틀 명')
+
+@section('css')
+    <link rel="stylesheet" href="{{asset('css/css파일 이름.css')}}">
+@endsection
+
+@section('contents')
+
+@endsection
+
+@section('js')
+    <script src="{{asset('js/js 파일 이름.js')}}"></script>
+@endsection
