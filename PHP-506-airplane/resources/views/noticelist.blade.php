e096ca5540648d521b66c5c2cb5effd77a5b18f7
ldh1103 <ldh517525@gmail.com>
2023-06-12 17:55:32 +0900

    0612

--- PHP-506-airplane/resources/views/noticelist.blade.php
+++ PHP-506-airplane/resources/views/noticelist.blade.php
@@ -3,13 +3,24 @@
 @section('title', '공지사항')
 
 @section('css')
-    {{-- <link rel="stylesheet" href="{{asset('css/css파일 이름.css')}}"> --}}
+    <link rel="stylesheet" href="{{asset('css/noticelist.css')}}">
 @endsection
 
 @section('contents')
-    <div class="container">
-        
-    </div>
+        <div class="nListContainer">
+            <div class="listInfo row">
+                <div class="col-2 textCenter afterLine">번호</div>
+                <div class="col-7 afterLine">제목</div>
+                <div class="col-3 textCenter">작성일</div>
+            </div>
+            <div class="listContents row">
+                {{-- TODO : DB select 후 foreach --}}
+                <div class="col-2 textCenter afterLine">1</div>
+                <div class="col-7 afterLine">제목제목제목</div>
+                <div class="col-3 textCenter">2023-06-12</div>
+            </div>
+        </div>
+            {{-- TODO : 페이징 --}}
 @endsection
 
 @section('js')
