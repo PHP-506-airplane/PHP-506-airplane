3bf4ab357c69235bcbd7ca790469112c2d76fd3f
ldh1103 <ldh517525@gmail.com>
2023-06-13 10:11:16 +0900

    0613

--- PHP-506-airplane/database/migrations/2023_06_13_100259_create_qna_reply_table.php
+++ PHP-506-airplane/database/migrations/2023_06_13_100259_create_qna_reply_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+        Schema::create('qna_reply', function (Blueprint $table) {
+            $table->id('reply_no');
+            $table->string('reply_content', 1000);
             $table->timestamps();
+            $table->softDeletes();
+            // TODO : qna_info 테이블 qna_no PK & FK 추가
+            // TODO : admin_info 테이블 adm_no FK 추가
         });
     }
 
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('qna_reply');
     }
 };
