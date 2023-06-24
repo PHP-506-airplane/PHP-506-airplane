34c826307a8d3d7f906934ca2bbce8b99c4ec7a1
LDH1103 <ldh517525@gmail.com>
2023-06-24 21:11:13 +0900

    0624 udt 탑승권 이동호

--- PHP-506-airplane/resources/views/myreservation.blade.php
+++ PHP-506-airplane/resources/views/myreservation.blade.php
@@ -22,32 +22,10 @@
 <button type="button" onclick="location.href='{{route('reservation.main')}}'">예약하기</button>
 @endif
 @foreach($data as $key => $val)
-{{-- {!!'flyno : ' . strtoupper($val['fly_no']) . '<br>'!!}
-{!!'resno : ' . strtoupper($val['reserve_no']) . '<br>'!!}
-{!!'편명 : ' . strtoupper($val['flight_num']) . '<br>'!!}
-{!!'항공사 : ' . $val['line_name'] . '<br>'!!}
-{!!'항공사 코드 : ' . $val['line_code'] . '<br>'!!}
-{!!'비행기 이름 : ' . $val['plane_name'] . '<br>'!!}
-{!!'예약자 이름 : ' . $val['u_name'] . '<br>'!!}
-{!!'좌석 : ' . $val['seat_no'] . '<br>'!!}
-{!!'날짜 : ' . $val['fly_date'] . '<br>'!!}
-{!!'출발 시간 : ' . $val['dep_time'] . '<br>'!!}
-{!!'도착 시간 : ' . $val['arr_time'] . '<br>'!!}
-{!!'소요 시간 : ' . TimeCalculation($val['dep_time'], $val['arr_time']) . '<br>'!!}
-{!!'출발 : ' . $val['dep_port_name'] . '<br>'!!}
-{!!'도착 : ' . $val['arr_port_name'] . '<br>'!!}
-<form action="{{route('reservation.rescancle')}}" method="POST" id="formCancel">
-@csrf
-<input type="hidden" name="reserve_no" value="{{$val['reserve_no']}}">
-<input type="hidden" name="t_no" value="{{$val['t_no']}}">
-<button type="button" onclick="confirmCancel()">예약 취소</button>
-<button type="submit">예약 취소</button>
-</form>
------------------------------------------------------------------------------------ --}}
 <div class="boarding-pass">
     <header>
         <div class="flight">
-            <strong>{{str_replace('항공', '', $val['line_name'])}}</strong>
+            <strong>{{$val['line_name']}}</strong>
         </div>
     </header>
     <section class="cities">
@@ -56,7 +34,7 @@
 
             <strong>{{strtoupper($val['dep_port_eng'])}}</strong>
         </div>
-        <div class="city cityRight">
+        <div class="city">
             <small>{{str_replace('공항', '' , $val['arr_port_name'])}}</small>
 
             <strong>{{strtoupper($val['arr_port_eng'])}}</strong>
