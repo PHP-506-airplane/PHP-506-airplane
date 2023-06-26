bbf3a2015eae42f1ba502430b35b605811285d58
SuyeonParka <sysysy3331@gmail.com>
2023-06-26 12:39:22 +0900

    0626

--- PHP-506-airplane/resources/views/layout/inc/header.blade.php
+++ PHP-506-airplane/resources/views/layout/inc/header.blade.php
@@ -8,13 +8,12 @@
 **************************************************/
 --}}
 
-<ul class="nav nav-pills justify-content-center">
+{{-- <ul class="nav nav-pills justify-content-center">
     <a class="navbar-brand" href="{{route('reservation.main')}}"><img width="150px" height="50px" class="air" src="{{asset('img/air.png')}}" alt="logo"></a>
         <div style="width:950px">
         </div>
         <li class="nav-item">
             <a class="nav-link p-3 mb-2 bg-body text-dark" href="{{route('notice.baggage')}}">수하물 안내</a>
-            {{-- <a class="nav-link p-3 mb-2 bg-body text-dark" href="{{route('notice.rateinfoget')}}">운임 안내</a> --}}
         </li>
     @guest
         <li class="nav-item">
@@ -39,44 +38,80 @@
         </li>
 
     @endauth 
-</ul>
+</ul> --}}
 
-
-{{-- <nav class="navbar navbar-expand-lg navbar-dark fixed-top navHeader" id="mainNav">
-    <div class="container">
-        {{-- <a class="navbar-brand" href="{{route('reservation.main')}}"><img src="{{asset('img/logo.png')}}" alt="logo" class="imgLogo"></a> --}}
-        {{-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            Menu
-            <li class="fas fa-bars ms-1"></li>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <div class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-                <button class="nav-item"><a href="#about">수하물 안내</a></button>
-                <button class="nav-item">
-                @guest
-                    <a href="{{route('users.login')}}">로그인</a>
-                    <a href="{{route('users.registration')}}">회원가입</a>
-                @endguest
-
-                @auth
-                    <button class="nav-item">
-                        <a href="">{{Auth::user()->u_name}}님</a>
-                    </button>
-                    <button class="nav-item">
-                        <div class="dropdown">
-                            <button onclick="dp_menu()" class="button">마이페이지</button>
-                            <div id="drop-content">
-                                <a hreaf='{{route('users.useredit')}}'>정보수정</a>
-                                <a hreaf='{{route('reservation.myreservation')}}'>예약조회</a>
-                            </div>
-                        </div>
-                    </button>
-                    <button class="nav-item">
-                        <a href="{{route('users.logout')}}">로그아웃</a>
-                    </button>
-                @endauth
-                </button>
-            </div>
-        </div>
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <div style="width:70px"></div>
+    <a class="navbar-brand" href="{{route('reservation.main')}}"><img width="180px" height="40px" class="air" src="{{asset('img/logoplz.png')}}" alt="logo"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item" style="font-weight:600">
+          <a class="nav-link active" aria-current="page" href="{{route('reservation.main')}}">Home</a>
+        </li>
+        @guest
+             <li class="nav-item justify-content-end" style="font-weight:600">
+            <a class="nav-link" href="{{route('notice.baggage')}}">수하물 안내</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="{{route('users.login')}}">로그인</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="{{route('users.registration')}}">회원가입</a>
+            </li>
+        @endguest
+       
+       @auth
+            <li class="nav-item justify-content-end" style="font-weight:600">
+                <a class="nav-link" href="{{route('notice.baggage')}}">수하물 안내</a>
+            </li>
+            <li class="nav-item dropdown justify-content-end" style="font-weight:600">
+             
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                {{Auth::user()->u_name}}님
+            </a>
+            <ul class="dropdown-menu justify-content-end" style="font-weight:600">
+                <li><a class="dropdown-item" href="{{route('users.useredit')}}">회원정보 수정</a></li>
+                <li><a class="dropdown-item" href="{{route('reservation.myreservation')}}">예약 조회</a></li>
+                <li><a class="dropdown-item" href="{{route('users.logout')}}">로그아웃</a></li>
+            </ul>
+            </li>
+       @endauth
+      </ul>
     </div>
-</nav>  --}}
+  </div>
+</nav>
+
+{{-- @section('css') 
+    <link rel="stylesheet" href="{{asset('css/header.css')}}">
+@endsection --}}
+
+ {{-- <nav class="navbar">
+        <div class="navbar__logo">
+            <i class="fab fa-apple"></i>
+            <a href="{{route('reservation.main')}}"><img width="150px" height="50px" class="air" src="{{asset('img/air.png')}}" alt="logo"></a>
+        </div>
+        @guest
+            <ul class="navbar__menu">
+                <li><a href="{{route('notice.baggage')}}">수하물 안내</a></li>
+                <li><a href="{{route('users.login')}}">로그인</a></li>
+                <li><a href="{{route('users.registration')}}">회원가입</a></li>
+            </ul>
+        @endguest
+               
+        @auth
+            <ul class="navbar__menu">
+                <li>{{Auth::user()->u_name}}님</li>
+                <li><a href="{{route('users.login')}}">로그인</a></li>
+                <li><a href="{{route('users.registration')}}">회원가입</a></li>
+            </ul>
+        @endauth
+        <a href="#" class="navbar__toggleBtn"><i class="fas fa-bars"></i></a>
+    </nav>
+
+@section('js')
+    <script src="{{asset('js/header.js')}}"></script>
+@endsection --}}
