9c8436814af65a98162afad51590bc99c2591ab5
SuyeonParka <sysysy3331@gmail.com>
2023-06-30 16:54:54 +0900

    0630

--- PHP-506-airplane/resources/views/login.blade.php
+++ PHP-506-airplane/resources/views/login.blade.php
@@ -19,25 +19,25 @@
 <div id="con">
     <div id="login">
         <div id="login_form">
-        <form action="{{route('users.login.post')}}" method="post">
-        @csrf 
-            <h3 class="login" style="letter-spacing:-1px;">로그인</h3>
-            <hr>
-            <label>
-            <p style="text-align: left; font-size:12px; color:#666">이메일</p>
-            <input type="text" placeholder="아이디를 입력하세요" class="size" name="u_email" id="u_email">
-            <p></p>
-            </label>
-            <br>
-            <label>
-            <p style="text-align: left; font-size:12px; color:#666">비밀번호</p>
-            <input type="password" placeholder="비밀번호를 입력하세요" class="size" name="u_pw" id="u_pw">
-            </label>
-            <div style="height:30px"></div>
-            <p>
-                <input type="submit" value="로그인" class="btn">
-            </p>
-        </form>
+            <form action="{{route('users.login.post')}}" method="post">
+            @csrf 
+                <h3 class="login" style="letter-spacing:-1px;">로그인</h3>
+                <hr>
+                <label class="loginlabel">
+                    <p style="text-align: left; font-size:12px; color:#666">이메일</p>
+                    <input type="text" placeholder="아이디를 입력하세요" class="size" name="u_email" id="u_email">
+                    <p></p>
+                </label>
+                <br>
+                <label class="loginlabel">
+                    <p style="text-align: left; font-size:12px; color:#666">비밀번호</p>
+                    <input type="password" placeholder="비밀번호를 입력하세요" class="size" name="u_pw" id="u_pw">
+                </label>
+                <div style="height:30px"></div>
+                <p>
+                    <input type="submit" value="로그인" class="btn">
+                </p>
+            </form>
         <hr>
         <p class="find">
             <span><a href="{{route('users.registration')}}">회원가입</a></span>
