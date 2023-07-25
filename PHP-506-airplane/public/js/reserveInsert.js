0d7f13b9c13033d8d92501a4474e28661eb343f8
SuyeonParka <sysysy3331@gmail.com>
2023-07-25 21:15:26 +0900

    0725

--- PHP-506-airplane/public/js/reserveInsert.js
+++ PHP-506-airplane/public/js/reserveInsert.js
@@ -16,8 +16,118 @@ async function getPrice(pk) {
 
 //항공편 pk, 좌석 이름을 보내서 캐시가 있는지 확인
 //reservationController의 caching으로 넘어감
-async function submitReq() {
+// async function submitReq() {
 
+//     let seatNo = document.querySelectorAll('.seat_no');
+//     let seatNo2 = document.querySelectorAll('.seat_no2');
+//     let flyNo = document.getElementById('fly_no');
+//     let flyNo2 = document.getElementById('fly_no2');
+
+//     let seats = [];
+//     let seats2 = [];
+
+//     seatNo.forEach(function(seat) {
+//         seats.push(seat.value);
+//     });
+
+//     seatNo2.forEach(function(seat) {
+//         seats2.push(seat.value);
+//     });
+
+//     console.log(seats);
+//     console.log(seats2);
+
+//     let arrCaching = [];
+//     let arrCaching2 = [];
+
+//     for(let i = 0; i < seats.length; i++) {
+//         arrCaching.push(await axios.post('/api/reservations/cache', {
+//             fly_no: flyNo.value,
+//             seat_no: seats[i]
+//         }));
+//         if (arrCaching[i].data.success) {
+//             let price1 = await getPrice(flyNo);
+//             let totalPrice = (price1 + price2) * allCnt.value;
+//             let cachedData=[];
+    
+//                 cachedData = [
+//                     [flyNo, seats[i]]
+//                 ];
+                
+//             requestPay(totalPrice, cachedData);
+//         } else {
+//             removeLoading();
+//             alert('이미 진행중인 예약입니다.');
+//         }
+//     }
+
+//     for(let i = 0; i < seats2.length; i++) {
+//         arrCaching2.push(await axios.post('/api/reservations/cache', {
+//             fly_no: flyNo2.value,
+//             seat_no: seats2[i]
+//         }));
+//         if (arrCaching2[i].data.success) {
+//             let price2 = await getPrice(flyNo2);
+//             let totalPrice = (price1 + price2) * allCnt.value;
+//             let cachedData=[];
+    
+//                 cachedData = [
+//                     [flyNo, seats[i]]
+//                 ];
+//                  cachedData = [
+//                     [flyNo2, seats2[i]]
+//                 ];
+//             requestPay(totalPrice, cachedData);
+//         } else {
+//             removeLoading();
+//             alert('이미 진행중인 예약입니다.');
+//         }
+//     }
+
+
+
+//     console.log(arrCaching);
+
+//     let allCnt = document.getElementById('allCnt');
+
+//     if (arrCaching.data.success && arrCaching2.data.success) {
+//         let price1 = await getPrice(flyNo);
+//         let price2 = await getPrice(flyNo2);
+//         let totalPrice = (price1 + price2) * allCnt.value;
+//         let cachedData=[];
+
+//             cachedData = [
+//                 [flyNo, seats[i]]
+//             ];
+//              cachedData = [
+//                 [flyNo2, seats2[i]]
+//             ];
+//         requestPay(totalPrice, cachedData);
+//     } else {
+//         removeLoading();
+//         alert('이미 진행중인 예약입니다.');
+//     }
+//     // let caching2 = await axios.post('/api/reservations/cache', {
+//     //     fly_no: fly_no2,
+//     //     seat_no: s_name2.value
+//     // });
+
+//     // if (caching.data.success && caching2.data.success) {
+//     //     let price1 = await getPrice(fly_no);
+//     //     let price2 = await getPrice(fly_no2);
+//     //     let totalPrice = price1 + price2;
+//     //     let cachedData = [
+//     //         [fly_no, s_name.value],
+//     //         [fly_no2, s_name2.value]
+//     //     ];
+//     //     requestPay(totalPrice, cachedData);
+//     // } else {
+//     //     removeLoading();
+//     //     alert('이미 진행중인 예약입니다.');
+//     // }
+// }
+
+async function submitReq() {
     let seatNo = document.querySelectorAll('.seat_no');
     let seatNo2 = document.querySelectorAll('.seat_no2');
     let flyNo = document.getElementById('fly_no');
@@ -37,54 +147,33 @@ async function submitReq() {
     console.log(seats);
     console.log(seats2);
 
-    let arrCaching = [];
-    let arrCaching2 = [];
-
-    for(let i = 0; i < seats.length; i++) {
-        arrCaching.push(await axios.post('/api/reservations/cache', {
-            fly_no: flyNo.value,
-            seat_no: seats[i]
-        }));
-        if (arrCaching[i].data.success) {
-            let price1 = await getPrice(flyNo);
-            let totalPrice = (price1 + price2) * allCnt.value;
-            let cachedData=[];
-    
-                cachedData = [
-                    [flyNo, seats[i]]
-                ];
-                
-            requestPay(totalPrice, cachedData);
-        } else {
-            removeLoading();
-            alert('이미 진행중인 예약입니다.');
+    // 중복된 좌석을 추적하기 위한 플래그 변수와 중복된 좌석 정보를 담을 배열을 생성.
+    let isDuplicate = false;
+    let duplicateSeats = [];
+    for (let i = 0; i < seats.length; i++) {
+        if (seats.indexOf(seats[i]) !== i) {
+            // 중복된 좌석이 발견되면 해당 좌석을 duplicateSeats 배열에 추가하고 플래그 변수를 true로 설정.
+            if (!duplicateSeats.includes(seats[i])) {
+                duplicateSeats.push(seats[i]);
+            }
+            isDuplicate = true;
         }
     }
 
-    for(let i = 0; i < seats2.length; i++) {
-        arrCaching2.push(await axios.post('/api/reservations/cache', {
-            fly_no: flyNo2.value,
-            seat_no: seats2[i]
-        }));
-        if (arrCaching2[i].data.success) {
-            let price2 = await getPrice(flyNo2);
-            let totalPrice = (price1 + price2) * allCnt.value;
-            let cachedData=[];
-    
-                cachedData = [
-                    [flyNo, seats[i]]
-                ];
-                 cachedData = [
-                    [flyNo2, seats2[i]]
-                ];
-            requestPay(totalPrice, cachedData);
-        } else {
-            removeLoading();
-            alert('이미 진행중인 예약입니다.');
+    for (let i = 0; i < seats2.length; i++) {
+        if (seats2.indexOf(seats2[i]) !== i) {
+            if (!duplicateSeats.includes(seats2[i])) {
+                duplicateSeats.push(seats2[i]);
+            }
+            isDuplicate = true;
         }
     }
 
-
+    if (isDuplicate) {
+        removeLoading();
+        alert('선택한 좌석 중에 중복된 좌석이 있습니다.');
+        return;
+    }
 
     console.log(arrCaching);
 
@@ -94,35 +183,17 @@ async function submitReq() {
         let price1 = await getPrice(flyNo);
         let price2 = await getPrice(flyNo2);
         let totalPrice = (price1 + price2) * allCnt.value;
-        let cachedData=[];
+        let cachedData = [];
 
-            cachedData = [
-                [flyNo, seats[i]]
-            ];
-             cachedData = [
-                [flyNo2, seats2[i]]
-            ];
+        cachedData = [
+            [flyNo, seats[i]]
+        ];
+        cachedData = [
+            [flyNo2, seats2[i]]
+        ];
         requestPay(totalPrice, cachedData);
     } else {
         removeLoading();
         alert('이미 진행중인 예약입니다.');
     }
-    // let caching2 = await axios.post('/api/reservations/cache', {
-    //     fly_no: fly_no2,
-    //     seat_no: s_name2.value
-    // });
-
-    // if (caching.data.success && caching2.data.success) {
-    //     let price1 = await getPrice(fly_no);
-    //     let price2 = await getPrice(fly_no2);
-    //     let totalPrice = price1 + price2;
-    //     let cachedData = [
-    //         [fly_no, s_name.value],
-    //         [fly_no2, s_name2.value]
-    //     ];
-    //     requestPay(totalPrice, cachedData);
-    // } else {
-    //     removeLoading();
-    //     alert('이미 진행중인 예약입니다.');
-    // }
 }
